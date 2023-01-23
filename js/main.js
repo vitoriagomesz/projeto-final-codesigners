42b9255b3c6cee6e0de4d7c792e3f777b78979fb
vitoriagomesz <vitoria.gomesdasilva@hotmail.com>
2023-01-23 18:24:55 -0300

    Javascript overlay > modal

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 //DECLARANDO VARIAVÉIS
+
 var btnContact = document.querySelector('.jl-btn-contact');
+var toggleModal = document.querySelectorAll('.jl-toggle-modal')
 
 //PAGE PRELOADER
+
 window.addEventListener('load', function () {
     var pagePreloader = document.querySelector('.jl-preloader');
     pagePreloader.classList.add('jl-fade-out');
@@ -20,3 +23,16 @@ btnContact.addEventListener('click', function() {
     boxContact.classList.toggle('jl-is-open');
     this.classList.toggle('jl-change-icon'); /* this se refere ao btnContact */
 });
+
+//ABRINDO E FECHANDO O MODAL DE ORÇAMENTO
+
+for (var i = 0; i < toggleModal.length; i++){
+    toggleModal[i].addEventListener('click', function() {
+        var overlay = document.querySelector('.jl-overlay');
+        var modalOrcamento = document.querySelector('#jl-modal-orcamento');
+
+        overlay.classList.toggle('jl-is-open');
+        modalOrcamento.classList.toggle('jl-is-open');
+        modalOrcamento.classList.toggle('jl-slide-top-in');
+    });
+}
